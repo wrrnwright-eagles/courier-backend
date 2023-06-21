366fc7e33ab83f3d0d0b7618f6cb590bffb37703
wrrnwright-eagles <warren.wright@eagles.oc.edu>
2023-06-20 21:23:11 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,50 @@
+require("dotenv").config();
+
+
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const db = require("./app/models");
+
+db.sequelize.sync()
+  .then(() => {
+    console.log('Database synchronized');
+  })
+  .catch((error) => {
+    console.error('Error synchronizing database:', error);
+  });
+
+var corsOptions = {
+  origin: "http://localhost:8081",
+};
+
+app.use(cors(corsOptions));
+app.options("*", cors());
+
+// parse requests of content-type - application/json
+app.use(express.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+// simple route
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to the courier service backend." });
+});
+
+require("./app/routes/auth.routes.js")(app);
+require("./app/routes/user.routes.js")(app);
+require("./app/routes/forgotPassword.routes")(app);
+require("./app/routes/subscribe.routes")(app);
+
+
+
+// set port, listen for requests
+const PORT = process.env.PORT || 3201;
+if (process.env.NODE_ENV !== "test") {
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}.`);
+  });
+}
+
+module.exports = app;
