26b6e38267585bdfe08ca72e1ce859619f5772d7
Your Github username <d.pope@eagles.oc.edu>
2023-07-08 14:31:36 -0500

    fix

--- app/models/order.model.js
+++ app/models/order.model.js
@@ -1,21 +1,40 @@
 module.exports = (sequelize, Sequelize) => {
-    const Order = sequelize.define("order", {
-      date: {
-        type: Sequelize.DATEONLY,
-        allowNull: false,
-      },
-      time: {
-        type: Sequelize.TIME,
-        allowNull: false,
-      },
-      blocks: {
-        type: Sequelize.INTEGER,
-        allowNull: true,
-      },
-      price: {
-        type: Sequelize.DECIMAL,
-        allowNull: false,
-      },
-    });
-    return Order;
-  };
+  const Order = sequelize.define("order", {
+    id: {
+      type: Sequelize.INTEGER,
+      autoIncrement: true,
+      allowNull: false,
+      primaryKey: true,
+    },
+    date: {
+      type: Sequelize.DATEONLY,
+      allowNull: false,
+    },
+    time: {
+      type: Sequelize.TIME,
+      allowNull: false,
+    },
+    blocks: {
+      type: Sequelize.INTEGER,
+      allowNull: true,
+    },
+    price: {
+      type: Sequelize.DECIMAL,
+      allowNull: false,
+    },
+    pickup: {
+      type: Sequelize.INTEGER,
+      allowNull: true,
+    },
+    delivery: {
+      type: Sequelize.INTEGER,
+      allowNull: true,
+    },
+    courierId: {
+      type: Sequelize.INTEGER,
+      allowNull: true,
+    },
+  });
+
+  return Order;
+};
